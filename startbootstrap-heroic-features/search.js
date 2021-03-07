21721a7d48de75405bb87f82422ee55d8c815051
jyothihr92 <fnu.jyothihassanramesh@sjsu.edu>
2021-03-07 01:22:41 -0800

    Added searchRoom.html and worked on effective rendering of the pages

--- startbootstrap-heroic-features/search.js
+++ startbootstrap-heroic-features/search.js
@@ -35,22 +35,50 @@ function search() {
     function generateSearchOutput(response){
       console.log('here')
       console.log('carddisplay = ' + JSON.stringify(document.querySelector('carddisplay')))
-       document.getElementById('carddisplay')
-        .innerHTML = response.data.data.reduce((a, res) => a + 
-        `<div class="col-lg-3 col-md-3 mb-4">
+      cardtext = "";
+      for (var i = 0; i < response.data.data.length; i++)
+      {
+        cardtext += `<div class="col-lg-3 col-md-3 mb-4">
         <div class="card h-70">
         <div class="card-body">
-        <h4 class="card-title" >${res.hotelName} </h4>
-        <p class="card-text" > ${res.roomTypeName }</p>
+        <h4 class="card-title" id = "cardtitle" >${response.data.data[i].hotelName} </h4>
+        <p class="card-text" id = "cardtext" > ${response.data.data[i].roomTypeName}</p>
         </div>
         <div class="card-footer">
-        <a href="#" class="btn btn-primary">Select</a>
+        <a href = "selectRoom.html" class="btn btn-primary" id = "select" onclick = "clickSelect()">Select</a>
         </div>
         </div>
-        </div>`,''
-      );
+        </div>`;
+      }
+      return document.getElementById('carddisplay')
+      .innerHTML = cardtext;
 
+      //  return document.getElementById('carddisplay')
+      //   .innerHTML = response.data.data.reduce((a, res) => a + 
+      //   `<div class="col-lg-3 col-md-3 mb-4">
+      //   <div class="card h-70">
+      //   <div class="card-body">
+      //   <h4 class="card-title" id = "cardtitle" >${res.hotelName} </h4>
+      //   <p class="card-text" id = "cardtext" > ${res.roomTypeName }</p>
+      //   </div>
+      //   <div class="card-footer">
+      //   <a href = "selectRoom.html" class="btn btn-primary" id = "select" onclick = "clickSelect()">Select</a>
+      //   </div>
+      //   </div>
+      //   </div>`,''
+      // );
+      /*cardttitle = document.getElementById('cardtitle').value
+      cardtext = document.getElementById('cardtext').value
+      console.log(cardtitle + cardtext)*/
+      //document.getElementById('select').onclick = clickSelect()
     }
 }
+
+function clickSelect() {
+  console.log('here')
+
+  location.href = "selectRoom.html"
+}
+
 
 
